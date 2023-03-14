e8336174ec80ca0f2181e5dee7b7d12e465957b7
8toAutomate <70109587+8toAutomate@users.noreply.github.com>
2023-03-14 09:56:41 -0400

    Detection and File Update Finished

--- src/DrinkingDetection.py
+++ src/DrinkingDetection.py
@@ -1,12 +1,12 @@
 import cv2
-import time
+import datetime
 import numpy as np
-
-vid = cv2.VideoCapture(0)
+import time
+import csv
+import plyer
 
 lowHSV = (45, 110, 0) #15 81 0
 highHSV = (100, 255, 255) #(45, 255, 255)
-
 
 mouth_cascade = cv2.CascadeClassifier('src\CascadeClassifiers\haar-cascade-files-master\haarcascade_mcs_mouth.xml')
 glasses_cascade = cv2.CascadeClassifier('src\CascadeClassifiers\haar-cascade-files-master\haarcascade_eye_tree_eyeglasses.xml')
@@ -15,11 +15,29 @@ def intersecting(f,s):
     posWidth = min(f[2], s[2]) > max(f[0], s[0])
     posHeight = min(f[3], s[3]) > max(f[1], s[1])
     return posHeight and posWidth
+        
+last = datetime.datetime.now() - datetime.timedelta(seconds=5) # timeout starts 5 minutes ago so it starts recording when program starts
 
+def recordDrink():
+    global last
+    if datetime.datetime.now() - last > datetime.timedelta(seconds=5):
+        print("timeout end")
+        with open("src/DrinkingTimes.csv", "a") as file:
+            writer = csv.writer(file)
+            now = datetime.datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            print(dt_string)
+            writer.writerow([dt_string])
+            cv2.putText(frame,'Good Job!',(20,20),0,4,(50,255,0))
+            last = now
+            #file.close();
+        
+            
 
+vid = cv2.VideoCapture(0)
 
 while(True):
-      
+        
     # Capture the video frame
     # by frame
     ret, frame = vid.read()
@@ -40,18 +58,17 @@ while(True):
 
     contours, _ = cv2.findContours(filtered, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-    max_area = 0;
     max_rect = []
 
-    for c in contours:
-        rect = cv2.boundingRect(c)
-        if rect[2] < 60 or rect[3] < 60: continue
-        if rect[2] * rect[3] > max_area:
-            max_rect = rect
-        x,y,w,h = max_rect
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
-        cv2.putText(frame,'bottle Detected',(x+w+10,y+h),0,0.5,(255,0,0))
 
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > 2000:
+            c = max(contours, key=cv2.contourArea)
+            x,y,w,h = cv2.boundingRect(c)
+            max_rect = [x,y,w,h]
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+            cv2.putText(frame,'bottle Detected',(x+w+10,y+h),0,0.5,(255,0,0))
 
     # detect eyes and mouth
 
@@ -77,9 +94,10 @@ while(True):
         mouth_area_coords = [mouth_area[0], mouth_area[1], mouth_area[0] + mouth_area[2], mouth_area[1] + mouth_area[3]]
         
         if intersecting(bottle_coords, mouth_area_coords):
-            print("DRINKING")
+            print("DRINKING DETECTED")
+            recordDrink()
+            #time.sleep(60000)
             
-
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
